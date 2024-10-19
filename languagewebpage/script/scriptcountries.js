4edaa1bb62f1f0767a5c9fc54b42d85d0aefcb54
bsdc-ion <30270519@bsdc.ac.uk>
2024-10-19 12:50:30 +0100

    Small updates into the javascript

--- languagewebpage/script/scriptcountries.js
+++ languagewebpage/script/scriptcountries.js
@@ -1,7 +1,23 @@
-console.log('iubire merge!')
-function addBox() {
-    console.log("test if working");
-}
+console.log('Java script running!');
 
-var clickBox = document.getElementById('france');
-clickBox.onclick = addBox;
+// Boxes // 
+const msg1 = document.getElementById("morning!");
+const msg2 = document.getElementById("pronunce1");
+const msg3 = document.getElementById("hitext!");
+const msg4 = document.getElementById("pronunce2");
+
+// Countries //
+const robt = document.getElementById("Romania");
+const frbt = document.getElementById("France");
+const pkbt = document.getElementById("Pakistan");
+const hubt = document.getElementById("Hungary");
+const bgbt = document.getElementById("Bulgaria");
+const ngbt = document.getElementById("Nigeria");
+
+// all translates // 
+const morningtr = ["Good Morning!", "Bonjur", "Buna dimineata", "صبح بخیر", "Добро утро", "Jó reggelt", "Kàárọ̀"];
+const hellostr = ["Hello!", "Bonjour!", "Salut!", "ہیلو!", "здравей!", "Üdvözlöm", "Witam", "Báwo"]
+
+robt.addEventListener("click", function () {
+    console.log(hellostr);
+});
