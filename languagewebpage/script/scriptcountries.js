0bf028041ce6685e4b34e37f297d3f3f4f315ba9
bsdc-ion <30270519@bsdc.ac.uk>
2024-10-22 13:54:23 +0100

    Countries added + bug fixes + changes based on feedback

--- languagewebpage/script/scriptcountries.js
+++ languagewebpage/script/scriptcountries.js
@@ -12,14 +12,16 @@ const robt = document.getElementById("Romania");
 const frbt = document.getElementById("France");
 const pkbt = document.getElementById("Pakistan");
 const hubt = document.getElementById("Hungary");
-const bgbt = document.getElementById("Bulgaria");
+const plbt = document.getElementById("Poland");
 const ngbt = document.getElementById("Nigeria");
+const wlbt = document.getElementById("Welsh");
+const lvbt = document.getElementById("Latvia");
 
 // all translates // 
-const morningtr = ["Good Morning!", "Bonne matinée!", "Buna dimineata!", "صبح بخیر!", "Добро утро!", "Jó reggelt!", "Kàárọ̀!"];
-const pron1 = ["[Good Morn·ing]", "[Buna dimi·neatsa]", "[Bon matin]", "[Sabah bak·hair]", "[Dobro utro]", "[Jo reg·et]", "[Karu]"];
-const hellostr = ["Hello!", "Bonjour!", "Salut!", "ہیلو!", "здравей!", "Üdvözlöm", "Báwo"];
-const pron2 = ["Hello", "Buna", "Bon·jur", "Helo", "Zdra·vei", "Üd·voz·lom", "Ba·vo"];
+const morningtr = ["Good Morning!", "Bonne matinée!", "Buna dimineata!", "Subha bakhair!", "Добро утро!", "Jó reggelt!", "Ekaaro", "Bore da!", "Labrīt!"];
+const pron1 = ["[Good Morn·ing]", "[Buna dimi·neatsa]", "[Bon matin]", "[Sabah bak·hair]", "[Dobro utro]", "[Jo reg·et]", "[Eca·ro]", "[Borra-da]", "[Labri·]"];
+const hellostr = ["Hello!", "Bonjour!", "Salut!", "As-salamu alaykum!", "Część!", "Szia", "Báwo", "Helo!", "Labdien!"];
+const pron2 = ["[Hello]", "[Buna]", "[Bon·jur]", "[Asalam ulake-um]", "[Chay-sht]", "[Szia]", "[Ba·vo]", "[Helo]", "[La·bien]"];
 
 // button work //
 robt.addEventListener("click", function () {
@@ -43,7 +45,7 @@ pkbt.addEventListener("click", function () {
     msg4.textContent = pron2[3];
 });
 
-bgbt.addEventListener("click", function () {
+plbt.addEventListener("click", function () {
     msg1.textContent = morningtr[4];
     msg3.textContent = hellostr[4];
     msg2.textContent = pron1[4];
@@ -62,4 +64,18 @@ ngbt.addEventListener("click", function () {
     msg3.textContent = hellostr[6];
     msg2.textContent = pron1[6];
     msg4.textContent = pron2[6];
+});
+
+wlbt.addEventListener("click", function () {
+    msg1.textContent = morningtr[7];
+    msg3.textContent = hellostr[7];
+    msg2.textContent = pron1[7];
+    msg4.textContent = pron2[7];
+});
+
+lvbt.addEventListener("click", function () {
+    msg1.textContent = morningtr[8];
+    msg3.textContent = hellostr[8];
+    msg2.textContent = pron1[8];
+    msg4.textContent = pron2[8];
 });
