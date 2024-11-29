d9f63377e41533af7a78149406410afc8898ad72
bsdc-ion <30270519@bsdc.ac.uk>
2024-11-29 13:52:18 +0000

    desprenoi page done

--- miculbrutar/script/scriptdesprenoi.js
+++ miculbrutar/script/scriptdesprenoi.js
@@ -1,6 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const homebutton = document.getElementById("produse");
-    homebutton.addEventListener("click", function () {
-        window.location.href = "indexproduse.html";
-    });
+
+/* navigatie */
+const homebutton = document.getElementById("produse");
+
+/* date imp */
+const locatie = document.getElementById("locatie");
+
+/* social */
+const ig = document.getElementById("insta");
+const fb = document.getElementById("Facebook");
+const lin = document.getElementById("Linkedin");
+const yt = document.getElementById("Youtube");
+const tw = document.getElementById("Twitter");
+
+/* navigatie */
+homebutton.addEventListener("click", function () {
+    window.location.href = "indexproduse.html";
+});
+
+/* date imp */
+locatie.addEventListener("click", function() {
+    window.open('https://maps.app.goo.gl/Ljcdk9ZfBYCzrYLH9');
+});
+
+
+/* retele sociale */
+ig.addEventListener("click", function() {
+    window.open('https://www.instagram.com/');
+});
+fb.addEventListener("click", function() {
+    window.open('https://www.facebook.com/');
+});
+lin.addEventListener("click", function() {
+    window.open('https://www.linkedin.com/');
+});
+yt.addEventListener("click", function() {
+    window.open('https://www.youtube.com/');
+});
+tw.addEventListener("click", function() {
+    window.open('https://www.x.com/');
 });
--- miculbrutar/script/scriptproduse.js
+++ miculbrutar/script/scriptproduse.js
@@ -1,10 +1,6 @@
 window.onload = function(){document.getElementsByTagName('html')[0].className = '';}
 
-document.addEventListener("DOMContentLoaded", function () {
-    const homebutton = document.getElementById("desprenoi");
-    homebutton.addEventListener("click", function () {
-        window.location.href = "indexdesprenoi.html";
-    });
+const homebutton = document.getElementById("desprenoi");
+homebutton.addEventListener("click", function () {
+    window.location.href = "indexdesprenoi.html";
 });
-
-
