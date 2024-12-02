0917f8c36615eeebaae633082fe9a116e14ea7bc
Madalin <30270519@bsdc.ac.uk>
2024-12-02 19:04:31 +0000

    script

--- miculbrutar/script/scriptprodus.js
+++ miculbrutar/script/scriptprodus.js
@@ -2,7 +2,6 @@
 const homebutton = document.getElementById("produse");
 const aboutbutton = document.getElementById("desprenoi");
 
-/* navigatie */
 homebutton.addEventListener("click", function () {
     window.location.href = "indexproduse.html";
 });
