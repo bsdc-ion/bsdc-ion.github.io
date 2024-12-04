2eef7c67ab93c588526d58907f31cb59a280c4f2
Madalin <30270519@bsdc.ac.uk>
2024-12-04 21:12:23 +0000

    script changed

--- miculbrutar/script/scriptproduse.js
+++ miculbrutar/script/scriptproduse.js
@@ -20,8 +20,6 @@ function readTextFile(file, callback) {
     }
     rawFile.send(null);
 }
-//
-var url = "http://127.0.0.1:5500/miculbrutar/indexprodus.html"
 
 // creaza conteiner 
 const conteiner = document.getElementById("produsee");
@@ -38,7 +36,13 @@ readTextFile("./produsele/produse.json", function(text){
         cutie.className = "produs";
         conteiner.appendChild(cutie);
 
-        // creaza imagine produs
+        cutie.innerHTML = `
+            <img src="${data[0].produse[i].image1}" alt="${data[0].produse[i].name}" class="imagineprodus">
+            <h3 class="numeprodus">${data[0].produse[i].name}</h3>
+            <button onclick="redirectToDetails('${data[0].produse[i].name}', '${data[0].produse[i].image1}')" class="maimultinfo">Informații</button>
+        `;
+
+        /*// creaza imagine produs
         var cutie1 = document.createElement("img");
         cutie1.src = data[0].produse[i].image1;
         cutie1.className = "imagineprodus";
@@ -49,15 +53,22 @@ readTextFile("./produsele/produse.json", function(text){
         namebar.className = "numeprodus";
         namebar.textContent = `${data[0].produse[i].name}`;
         cutie.appendChild(namebar);
+        
 
         // creaza buton
         var namebar2 = document.createElement("button");
         namebar2.className = "maimultinfo";
         namebar2.textContent = "Informatii";
         namebar2.id = `produs${i+1}`;
-
-        infobut[i] = namebar2.id;
         
-        cutie.appendChild(namebar2);
+        cutie.appendChild(namebar2);*/
     }
 });
+
+// Encode data into the URL
+function redirectToDetails(name, image, description) {
+    const url = new URL('/miculbrutar/indexprodus.html', window.location.origin);
+    url.searchParams.append('name', name);
+    url.searchParams.append('image', image);
+    window.location.href = url;
+}
