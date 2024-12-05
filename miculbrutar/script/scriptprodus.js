8aadfea5445cb617557bf7a07f770bc54b916fd3
Madalin <30270519@bsdc.ac.uk>
2024-12-05 22:39:28 +0000

    website almost done

--- miculbrutar/script/scriptprodus.js
+++ miculbrutar/script/scriptprodus.js
@@ -11,20 +11,7 @@ aboutbutton.addEventListener("click", function () {
 });
 
 
-// get data from other site
-function LoadQuery(param) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-}
-
-const name = LoadQuery('name');
-const image = LoadQuery('image');
-
-console.log(name);
-console.log(image);
-
-
-// recomandari 
+// functie de incarcat
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -37,6 +24,103 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 
+
+// get data from other site
+function LoadQuery(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+const name = LoadQuery('name');
+
+const img1 = document.getElementById("imaginemare");
+const img2 = document.getElementById("imaginimici");
+
+const titlu = document.getElementById("titlu");
+const descriere = document.getElementById("infobox");
+
+titlu.textContent = name;
+
+// recomandari
+const recom = document.getElementById("recomandari");
+
+var data;
+var lastname = [];
+
+readTextFile("./produsele/produse.json", function(text){
+    data = JSON.parse(text);
+
+    // variabile //
+    var results = [];
+    var searchField = "name";
+    var searchVal = titlu.textContent;
+
+    // cauta  valorea //
+    for (var i=0 ; i < 12 ; i++)
+    {
+        if (data[0].produse[i].name == searchVal) {
+            results.push(data[0].produse[i]);
+            break;
+        }
+    }
+
+    // continua incarcarea //
+        // imagini
+    var imgg1 = document.createElement("img");
+    imgg1.className = "imagineprin";
+    imgg1.src = results[0].image1;
+    img1.appendChild(imgg1);
+
+    var imgg2 = document.createElement("img");
+    imgg2.className = "imaginemica";
+    imgg2.src = results[0].image2;
+    img2.appendChild(imgg2);
+    
+    var imgg3 = document.createElement("img");
+    imgg3.className = "imaginemica";
+    imgg3.src = results[0].image3;
+    img2.appendChild(imgg3);
+
+    var imgg4 = document.createElement("img");
+    imgg4.className = "imaginemica";
+    imgg4.src = results[0].image4;
+    img2.appendChild(imgg4);
+
+        // descriere
+    var para = document.createElement("div");
+    para.className = "descirere";
+    para.textContent = results[0].description;
+    descriere.appendChild(para);
+
+
+    for(var i = 0; i < 3; i++){
+        var rand = getRandomInt(1, 11);
+
+        if(data[0].produse[rand].name == results[0].name || lastname[i-1] == results[0].name || lastname[i-1] == data[0].produse[rand].name) {
+            if(rand != 12) {
+                rand++;
+            }
+            else if(rand == 12) {
+                rand = getRandomInt(1, 8);
+            }
+        }
+
+        // creaza cutie support
+        var cutie = document.createElement("div");
+        cutie.className = "produs";
+        recom.appendChild(cutie);
+        
+        lastname.push(data[0].produse[rand].name);
+
+        // adauga restu de script
+        cutie.innerHTML = `
+            <img src="${data[0].produse[rand].image1}" alt="${data[0].produse[rand].name}" class="imagineprodus">
+            <h3 class="numeprodus">${data[0].produse[rand].name}</h3>
+            <button onclick="redirectToDetails('${data[0].produse[rand].name}', '${data[0].produse[rand].image1}')" class="maimultinfo">Informații</button>
+        `;
+    }
+});
+
+// genereaza numar random
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -44,22 +128,9 @@ function getRandomInt(min, max) {
 }
 
 
-const recom = document.getElementById("recomandari");
-
-readTextFile("./produsele/produse.json", function(text){
-    data = JSON.parse(text);
-
-    for(var i = 0; i < 3; i++) {
-        // genereaza numar random
-        var rand = getRandomInt(1, 12);
-
-        // adauga scriptu
-        recom.innerHTML = `
-            <div class="produs">
-                <img src="${data[0].produse[rand].image1}" alt="${data[0].produse[rand].name}" class="imagineprodus">
-                <h3 class="numeprodus">${data[0].produse[rand].name}</h3>
-                <button onclick="redirectToDetails('${data[0].produse[rand].name}', '${data[0].produse[rand].image1}')" class="maimultinfo">Informații</button>
-            </div>
-        `;
-    }
-});
+// codeaza tot in site
+function redirectToDetails(name, image, description) {
+    const url = new URL('/miculbrutar/indexprodus.html', window.location.origin);
+    url.searchParams.append('name', name);
+    window.location.href = url;
+}
--- miculbrutar/script/scriptproduse.js
+++ miculbrutar/script/scriptproduse.js
@@ -42,34 +42,12 @@ readTextFile("./produsele/produse.json", function(text){
             <h3 class="numeprodus">${data[0].produse[i].name}</h3>
             <button onclick="redirectToDetails('${data[0].produse[i].name}', '${data[0].produse[i].image1}')" class="maimultinfo">Informații</button>
         `;
-
-        /*// creaza imagine produs
-        var cutie1 = document.createElement("img");
-        cutie1.src = data[0].produse[i].image1;
-        cutie1.className = "imagineprodus";
-        cutie.appendChild(cutie1);
-
-        // creaza nume
-        var namebar = document.createElement("h1");
-        namebar.className = "numeprodus";
-        namebar.textContent = `${data[0].produse[i].name}`;
-        cutie.appendChild(namebar);
-        
-
-        // creaza buton
-        var namebar2 = document.createElement("button");
-        namebar2.className = "maimultinfo";
-        namebar2.textContent = "Informatii";
-        namebar2.id = `produs${i+1}`;
-        
-        cutie.appendChild(namebar2);*/
     }
 });
 
-// Encode data into the URL
+// codeaza tot in site
 function redirectToDetails(name, image, description) {
     const url = new URL('/miculbrutar/indexprodus.html', window.location.origin);
     url.searchParams.append('name', name);
-    url.searchParams.append('image', image);
     window.location.href = url;
 }
