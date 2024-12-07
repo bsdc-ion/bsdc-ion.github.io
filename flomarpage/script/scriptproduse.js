0f484869c54698fe290cc37e39962f6a17fc485f
Madalin <30270519@bsdc.ac.uk>
2024-12-07 23:42:46 +0000

    New portofolio page + removed some useless sites

--- flomarpage/script/scriptproduse.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const homebutton = document.getElementById("acasa");
-    homebutton.addEventListener("click", function () {
-        window.location.href = "index.html";
-    });
-});
