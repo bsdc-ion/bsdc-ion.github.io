ca46b938d3594b35a50a1b34bbe1f4d45fbcc86d
Madalin <30270519@bsdc.ac.uk>
2024-12-08 10:59:06 +0000

    Small update to portofolio page

---
+++ script/script.js
@@ -0,0 +1,28 @@
+console.log("Scripted by MLGBD");
+
+/* start of everything */
+const ehub = document.getElementById("experience");
+const pv1 = document.getElementById("portofoliov1");
+const trans = document.getElementById("translator");
+const pokemon = document.getElementById("pokemon");
+const miculbrutar = document.getElementById("miculbrutar");
+
+ehub.addEventListener("click", function() {
+    window.location.href = "/experiencehubpage/index";
+});
+
+pv1.addEventListener("click", function () {
+    window.location.href = "/oldportofolio/index";
+});
+
+trans.addEventListener("click", function () {
+    window.location.href = "/languagewebpage/indexcountries";
+});
+
+pokemon.addEventListener("click", function () {
+    window.location.href = "/pokemon/index";
+});
+
+miculbrutar.addEventListener("click", function () {
+    window.location.href = "/miculbrutar/index";
+});
