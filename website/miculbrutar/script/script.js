49e0592e50590c7a786b086af441fbdd0ec80f19
Madalin <30270519@bsdc.ac.uk>
2025-01-13 14:06:57 +0000

    idk

--- website/miculbrutar/script/script.js
+++
@@ -1,4 +0,0 @@
-const exploreButton = document.getElementById("explorebutton");
-exploreButton.addEventListener("click", function () {
-    window.location.href = "indexproducts.html";
-});
--- website/miculbrutar/script/scriptproduse.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){document.getElementsByTagName('html')[0].className = '';}
-
-const homebutton = document.getElementById("desprenoi");
-homebutton.addEventListener("click", function () {
-    window.location.href = "indexaboutus.html";
-});
