9836cb33a120d5a6ecd1be7066daab45f7f9513a
Madalin <nedelcu.madalin@icloud.com>
2025-07-06 20:31:56 +0100

    more changes

--- FixFolio/updatedate.js
+++ FixFolio/updatedate.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = './MyRepairs/index.html';
+const path2 = './index.html';
 
 let html = fs.readFileSync(path, 'utf8');
+let html2 = fs.readFileSync(path2, 'utf8');
+
 const now = new Date().toISOString();
 
 const updatedHtml = html.replace(
@@ -9,5 +12,11 @@ const updatedHtml = html.replace(
   `<div class="stat-number" id="last-updated">${now}</div>`
 );
 
+const updatedHtml2 = html2.replace(
+  /<div class="stat-number" id="last-updated">6 July<\/div>/,
+  `<div class="stat-number" id="last-updated">${now}</div>`
+);
+
 fs.writeFileSync(path, updatedHtml, 'utf8');
+fs.writeFileSync(path2, updatedHtml2, 'utf8');
 console.log("✅ HTML date updated to:", now);
